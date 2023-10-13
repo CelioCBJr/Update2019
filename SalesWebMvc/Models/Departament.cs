8b7216e80e8ea145883e473272d2278a8d0dc704
CelioCBJr <celio.borges@gmail.com>
2023-10-13 17:34:36 -0300

    Other entities and second migration

--- SalesWebMvc/Models/Departament.cs
+++ SalesWebMvc/Models/Departament.cs
@@ -1,8 +1,32 @@
-﻿namespace SalesWebMvc.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Models
 {
     public class Departament
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+        public Departament()
+        {
+        }
+
+        public Departament(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSaller(Seller seller)
+        {
+            Seller.Add(seller);
+        }
+        
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TatalSales(initial, final));
+        }
     }
 }
---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public double BaseSalary { get; set; }
+        public  Departament Departament { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+        public Seller()
+        {
+        }
+
+        internal static void Add(Seller seller)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Departament departament)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Departament = departament;
+        }
+        public void AddSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+        public void RemoveSales(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+        public double TatalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(SalesRecord => SalesRecord.Amount);
+        }
+    }
+}
